2288f00c135417469a0ccc23e24b7eba3a915be9
KlVnMrT <96387518+CatVooDoo@users.noreply.github.com>
2026-04-28 15:29:20 +0300

    Remove base path from Vite configuration
    Removed the base path configuration for the project.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/marg.in-protfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
